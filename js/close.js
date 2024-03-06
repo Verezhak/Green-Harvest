3d54c6044643e1de4e1bcea26bf09614be639721
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-03-06 22:42:59 +0100

    up

--- js/close.js
+++ js/close.js
@@ -1,33 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.menu-link, .button-menu-mobile');
-    const backdrop = document.querySelector('.backdrop');
-    const menuMobile = document.querySelector('.menu-mobile');
-    const menuButton = document.querySelector('.btn-menu');
-
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            menuMobile.classList.remove('is-open');
-            backdrop.classList.remove('is-open');
-        });
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.menu-link, .button-menu-mobile');
+  const backdrop = document.querySelector('.backdrop');
+  const menuMobile = document.querySelector('.menu-mobile');
+  const menuButton = document.querySelector('.btn-menu');
+  links.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      menuMobile.classList.remove('is-open');
+      backdrop.classList.remove('is-open');
     });
-
-    menuButton.addEventListener('click', function() {
-        const isOpen = menuMobile.classList.contains('is-open');
-        if (isOpen) {
-            menuMobile.classList.remove('is-open');
-            backdrop.classList.remove('is-open');
-        } else {
-            menuMobile.classList.add('is-open');
-            backdrop.classList.add('is-open');
-        }
-    });
-
-    document.querySelector('.btn-close').addEventListener('click', function() {
-        menuMobile.classList.remove('is-open');
-        backdrop.classList.remove('is-open');
-    });
-
-    const header = document.querySelector('.top');
+  });
+  menuButton.addEventListener('click', function () {
+    const isOpen = menuMobile.classList.contains('is-open');
+    if (isOpen) {
+      menuMobile.classList.remove('is-open');
+      backdrop.classList.remove('is-open');
+    } else {
+      menuMobile.classList.add('is-open');
+      backdrop.classList.add('is-open');
+    }
+  });
+  document.querySelector('.btn-close').addEventListener('click', function () {
+    menuMobile.classList.remove('is-open');
+    backdrop.classList.remove('is-open');
+  });
+    
+    
+      const header = document.querySelector('.top');
     const sectionWork = document.querySelector('.section-work');
 
     function handleHeaderStyle() {
